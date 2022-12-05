ffc2932b60daea748af67e5f480d504bc12f655a
mbilawalashraf <48543819+mbilawalashraf@users.noreply.github.com>
2022-12-05 16:46:46 +0400

    slider and table section added

---
+++ src/Assets/components/slider/data.js
@@ -0,0 +1,58 @@
+const people = [
+    {
+        id:1,
+        image: "https://ncx.cx/slide-img1.png",
+        name: "Cutting-Edge Trading Tools",
+        tag1: "Over 80 Trading Pairs",
+        tag2: "Fiat/Crypto Trading",
+        tag3: "Margin Trading",
+        tag4: "Market Depth",
+        tag5: "Customizable Interface",
+        tag6: "Grouping of Volumes",
+        tag7: "FOK, IOC, Limit Orders",
+        tag8: "",
+        tag9: "",
+        icon: "https://ncx.cx/slide-icon1.svg",
+        content: "Trading Crypto just got easier with NCX Cutting-Edge Trading Tools. State-of-the-ArtTechnology providing multi-level market depth, institutional grade liquidity, stable platform and fast processing and executions. Sign up and complete KYC to receive 10,000 NERO Tokens Airdrop and Earn a further 10,000 NERO on each referral through NCX Referral Program",
+        button: "Sign Up & Receive $NERO Airdrop",
+        url: "https://ncx.cx"
+    },
+    {
+        id:2,
+        image: "https://ncx.cx/slide-img2.png",
+        name: "Swap, Stake, Farm Yield on NCX Multichain DEX",
+        tag1: "Multichain Swap",
+        tag2: "DeFi Staking",
+        tag3: "Yield Farms",
+        tag4: "Liquidity Pools",
+        tag5: "Binance Smart Chain",
+        tag6: "Ethereum",
+        tag7: "Polygon",
+        tag8: "Fantom",
+        tag9: "Solana",
+        icon: "https://ncx.cx/slide-icon1.svg",
+        content: "Access Breakthrough dApps and DeFi Solutions through the NXC’s NeroSwap DEX & DeFi Ecosystem. Swap, Stake, Farm Yield across 5 leading Blockchains including Binance Smart Chain, Ethereum, Solana, Fantom and Polygon",
+        button: "Launch d’Apps",
+        url: "https://ncx.cx"
+    },
+    {
+        id:3,
+        image: "https://ncx.cx/slide-img3.png",
+        name: "Peace of mind with Advanced Security & 24/7 Support",
+        tag1: "2FA Authentication",
+        tag2: "Anti Phishing",
+        tag3: "Device Management",
+        tag4: "24/7 Support",
+        tag5: "APY Keys",
+        tag6: "Address Whitelisting",
+        tag7: "",
+        tag8: "",
+        tag9: "",
+        icon: "https://ncx.cx/slide-icon1.svg",
+        content: "An industry specific management tool that tackles the needs of compliance related systems. Star Compliance provides administrators with the tools to take control of their daily internal operations and site management across multiple venues.",
+        button: "Signing Up to Receive Airdrop",
+        url: "https://ncx.cx"
+    }
+]
+
+export default people;
