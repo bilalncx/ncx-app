43dd012edc7710a64dc85a11c95de788ea81246f
mbilawalashraf <48543819+mbilawalashraf@users.noreply.github.com>
2022-12-27 15:54:59 +0400

    cookie policy update

--- src/Assets/pages/cookie-policy/cookie-policy.jsx
+++ src/Assets/pages/cookie-policy/cookie-policy.jsx
@@ -15,15 +15,15 @@ function PrivacyPolicy(){
             <div className="policy-body container">
                 <h2>Cookie Policy</h2>
                 
-                <h3>I. GENERAL</h3>
+                <h3>1. GENERAL</h3>
                 
                 <p>This Cookie Policy governs use of cookies on the website www.ncx.cx (hereinafter – the “Website”) By visiting our Website, and any subdomains of this Website, you are accepting and consenting to the practices described in this Cookie Policy.</p>
                 
-                <h3>II. WHAT ARE COOKIES?</h3>
+                <h3>2. WHAT ARE COOKIES?</h3>
                 
                 <p>Cookies are small data files that are downloaded to a user’s computer, phone or tablet when you visit a website. Most web browsers automatically accept cookies. They help website operators to, for example, recognize a user that has visited a website previously and to analyze behavior in order to improve the user experience. Further details on the cookies used on our Website and their purposes are set out below.</p>
                 
-                <h3>III. WHAT COOKIES WILL BE USED AND FOR WHAT PURPOSE?</h3>
+                <h3>3. WHAT COOKIES WILL BE USED AND FOR WHAT PURPOSE?</h3>
                 
                 <p><strong>Performance cookies</strong></p>
                 
@@ -41,35 +41,35 @@ function PrivacyPolicy(){
                 
                 <p>These cookies enable our Website to remember choices you make and your personal preferences such as log in details in order to improve the user experience, for example by avoiding you having to re-enter these details on each visit to our Website.</p>
                         
-                <h3>IV. HOW LONG WILL THE COOKIES REMAIN?</h3>
+                <h3>4. HOW LONG WILL THE COOKIES REMAIN?</h3>
 
                 <p>The single session cookie is a temporary cookie which remains in the cookie file of your browser until you close the browser.</p>
                 
                 <p>The other cookies will remain in the cookie file of your browser after the closing of the browser, and will become active again when you reopen the Website. These cookies have different expiration dates. Following expiry of a cookie, a new version of that cookie will be downloaded when you next visit the Website, unless you have withdrawn your consent in the meantime.</p>
                 
-                <h3>V. THIRD PARTY COOKIES</h3>
+                <h3>5. THIRD PARTY COOKIES</h3>
                 
                 <p>Some cookies are provided on our behalf by trusted third party service providers to aid with user experience, reporting of user behavior, market research, providing advertisements according to user’s interests and improving the Website functionality for example. This user behavior is analyzed in order to provide an improved user experience.</p>
                 
-                <h3>VI. HOW DO I TURN COOKIES OFF?</h3>
+                <h3>6. HOW DO I TURN COOKIES OFF?</h3>
 
                 <p>You do not have to accept cookies, but if you opt to turn off or delete cookies generated via our Website, it may not perform as it has been designed to, possibly reducing the functionality on our Website, the options available to you, as well as the performance of our Website during your visit.</p>
                 
                 <p>All modern browsers allow you to change your cookie settings. These settings will typically be found in the ‘options’ or ‘preferences’ menu of your browser.</p>
                 
-                <h3>VII. USEFUL LINKS</h3>
+                <h3>7. USEFUL LINKS</h3>
 
                 <p>If you would like to find out more information about the usage of cookies on the Internet, please see the following links: www.allaboutcookies.org or www.youronlinechoices.eu which gives information about privacy issues around internet advertising.</p>
 
-                <h3>VIII. UPDATES TO THIS COOKIE POLICY</h3>
+                <h3>8. UPDATES TO THIS COOKIE POLICY</h3>
                 
                 <p>This Cookie Policy may be revised, modified, updated and/or supplemented at any time, without prior notice, at our sole discretion. When we make changes to this Cookie Policy, we will notify all users on our Website, and make the amended Cookie Policy available on our Website.</p>
 
-                <h3>IX. RELATION TO THE CUSTOMER AGREEMENT</h3>
+                <h3>9. RELATION TO THE CUSTOMER AGREEMENT</h3>
                 
                 <p>This Cookie Policy serves as an integral part of the Customer agreement. However, its acceptance is performed by visiting our Website and any subdomains of the Website.</p>
 
-                <h3>X. CONTACT</h3>     
+                <h3>10. CONTACT</h3>     
                 
                 <p>If you have any questions regarding our Cookie Policy, please contact us at support@ncx.cx.</p>
             </div>
