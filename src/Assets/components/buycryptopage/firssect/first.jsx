3d144a83c01ec45153d7545fcf5781f582080d33
mbilawalashraf <48543819+mbilawalashraf@users.noreply.github.com>
2023-02-17 11:38:09 +0400

    buy crypto page changes

--- src/Assets/components/buycryptopage/firssect/first.jsx
+++ src/Assets/components/buycryptopage/firssect/first.jsx
@@ -2,11 +2,54 @@ import React from "react";
 import { AnimationOnScroll } from 'react-animation-on-scroll';
 import CountUp from 'react-countup';
 import NCXCryptoBuySell from '../../../images/ncx-crypto-trading.png';
+import { ReactComponent as TrendingGraph } from '../../../images/ncx-trendinggraph.svg';
+import { ReactComponent as Ethereum } from '../../../images/ncx-ethereum.svg';
+import { ReactComponent as Ripple } from '../../../images/ncx-xrp.svg';
+import { ReactComponent as ChangeValue } from '../../../images/cryptochange.svg';
 
 function BuyCryptoFirstPage(){
     return(
         <div className="currencies-options row">
                 <img className="buy-ss22" src={NCXCryptoBuySell} alt="NCX Buy Sell Cryptos" />
+                <div id="mobile-shoss" className="col coinsect">
+                    <h1>Buy & Sell Crypto Instantly</h1>
+                    <p>Buy & Sell Crypto in just few clicks using Credit/Debit Card, Apple Pay, Google Pay and Wire Transfer. Simply enter the currency & amount and chose among the available payment methods, the best available rate is displayed by default, you can easily switch between providers and payment methods and chose the one right for you.</p>
+                    <div className="crypto-assets row">
+                        <div className="col coin-details">
+                            <div className="coin-graph">
+                                <Ethereum />
+                                <TrendingGraph />
+                            </div>
+                            <h1>Ethereum</h1>
+                            <p>0.382 USD</p>
+                            <p className="change">
+                                <ChangeValue />
+                                2.33%</p>
+                        </div>
+                        <div className="col coin-details">
+                            <div className="coin-graph">
+                                <Ripple />
+                                <TrendingGraph />
+                            </div>
+                            <h1>XRP</h1>
+                            <p>0.382 USD</p>
+                            <p className="change">
+                                <ChangeValue />
+                                4.33%</p>
+                        </div>
+                        <div className="col coin-details">
+                            <div className="coin-graph">
+                                <Ethereum />
+                                <TrendingGraph />
+                            </div>
+                            <h1>BNBH</h1>
+                            <p>0.382 USD</p>
+                            <p className="change">
+                                <ChangeValue />
+                                2.33%</p>
+                        </div>
+                    </div>
+                </div>
                 <div className="bar-grade33">
                     <AnimationOnScroll animateIn="animate__slideInLeft">
                         <div className="gradientbar"></div>
