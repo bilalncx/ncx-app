7ba7fdac59cdb6b0aac77c28baa61ea9f131b425
mbilawalashraf <48543819+mbilawalashraf@users.noreply.github.com>
2023-04-06 10:00:01 +0400

    token video update

--- src/Assets/components/ncxToken/section-1/first.jsx
+++ src/Assets/components/ncxToken/section-1/first.jsx
@@ -15,47 +15,62 @@ function FirstSect()
     const { t } = useTranslation();
 
     return(
-        <div className="token_first_sect">
-            <div className="container">
-                <div className="row txxss">
-                    <div className="col txt112">
-                        <h1>{t("next_token_powering_world_first_hybrid_cex_dex_trading_platform_defi_ecosystem")}</h1>
-                        <div className="btnss">
-                            <a href="#" className="token_signin">{t("token_dashboard_sign_in")}</a>
-                            <a href="#" className="claim_airdrop">{t("claim_airdrop")}</a>
+        <div className="ncx_token_aa">
+            <div id="ncxtokenVieo">
+                <video 
+                    id='token_video'
+                    autoPlay={true}
+                    muted={true}
+                    loop={true}
+                    playsInline={true}
+                    preload="auto"
+                    poster="../../../images/Token_bg.jpg"
+                    >
+                    <source src="https://ncx.cx/images/ncx-token-video-2.mp4" type="video/mp4" />
+                </video>
+            </div>
+            <div className="token_first_sect">
+                <div className="container">
+                    <div className="row txxss">
+                        <div className="col txt112">
+                            <h1>{t("next_token_powering_world_first_hybrid_cex_dex_trading_platform_defi_ecosystem")}</h1>
+                            <div className="btnss">
+                                <a href="#" className="token_signin">{t("token_dashboard_sign_in")}</a>
+                                <a href="#" className="claim_airdrop">{t("claim_airdrop")}</a>
+                            </div>
+                            <p>{t("sign_up_today_and_get_4000_ncx_tokens_airdrop")}</p>
                         </div>
-                        <p>{t("sign_up_today_and_get_4000_ncx_tokens_airdrop")}</p>
+                        <div className="col"></div>
                     </div>
-                    <div className="col"></div>
-                </div>
-                <div className="row slider_countdown">
-                    <div className="col">
-                        <RoundSlider />
-                    </div>
-                    <div className="col time_contribute">
-                        <div className="bonus_col">
-                            <p>{t("current_bonus")}</p>
-                            <h1>5-25%</h1>
-                            <p className="contributors">{t("contributor_can_receive")}</p>
+                    <div className="row slider_countdown">
+                        <div className="col">
+                            <RoundSlider />
                         </div>
-                        <Timer />
-                    </div>
-                </div>
-                <div className="how_and_why22">
-                    <div className="ds22">
-                        <div className="work">
-                            <a href="#"><HowWork /></a>
-                            <a href="#"><p>{t("how_it_work")}</p></a>
-                        </div>
-                        <div className="work">
-                            <a href="#"><WhyBuyToken /></a>
-                            <a href="#"><p>{t("why_buy_tokens_now")}</p></a>
+                        <div className="col time_contribute">
+                            <div className="bonus_col">
+                                <p>{t("current_bonus")}</p>
+                                <h1>5-25%</h1>
+                                <p className="contributors">{t("contributor_can_receive")}</p>
+                            </div>
+                            <Timer />
                         </div>
                     </div>
-                    <div className="social-col">
-                        <a href="https://www.instagram.com/ncx.cx/" target="_blank" rel="noreferrer"><Instagram /></a>
-                        <a href="#"><Telegram /></a>
-                        <a href="https://twitter.com/NCX_CX" target="_blank" rel="noreferrer"><Twitter /></a>
+                    <div className="how_and_why22">
+                        <div className="ds22">
+                            <div className="work">
+                                <a href="#"><HowWork /></a>
+                                <a href="#"><p>{t("how_it_work")}</p></a>
+                            </div>
+                            <div className="work">
+                                <a href="#"><WhyBuyToken /></a>
+                                <a href="#"><p>{t("why_buy_tokens_now")}</p></a>
+                            </div>
+                        </div>
+                        <div className="social-col">
+                            <a href="https://www.instagram.com/ncx.cx/" target="_blank" rel="noreferrer"><Instagram /></a>
+                            <a href="#"><Telegram /></a>
+                            <a href="https://twitter.com/NCX_CX" target="_blank" rel="noreferrer"><Twitter /></a>
+                        </div>
                     </div>
                 </div>
             </div>
