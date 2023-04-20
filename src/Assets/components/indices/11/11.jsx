82a20299a6e16919d40269a2cf8bac406ae0cafa
mbilawalashraf <48543819+mbilawalashraf@users.noreply.github.com>
2023-04-20 17:32:00 +0400

    new changes

--- src/Assets/components/indices/11/11.jsx
+++ src/Assets/components/indices/11/11.jsx
@@ -10,6 +10,31 @@ function IndicesFAQ()
                 <h1 className="fg__rer">FAQ</h1>
                 <div className="accord">
                     <details>
+                        <summary>How do Derivatives work?
+                            <span className='blue'>
+                                <DropIcon />
+                            </span>
+                        </summary>
+                        <div className='contt'>
+                            <p>When you trade Derivatives, you don’t actually own the underlying asset. Instead, you are entering into a financial contract to speculate on how high or low the price of the asset will go. If the price moves in your favour, you will make a profit. The risk is that it can also move against you so you will have a losing trade.</p>
+                            <p>Derivatives are a leveraged product, so you only need to put down a small deposit of money (margin) into your trading account to open a position. Using margin gives you the potential to make a much larger profit (or loss) than if you were to trade the asset outright. The use of leverage means a higher risk of losing more than your initial deposit.</p>
+                        </div>
+                    </details>
+                    <details>
+                        <summary>Which assets can I trade?
+                            <span className='blue'>
+                                <DropIcon />
+                            </span>
+                        </summary>
+                        <div className='contt'>
+                            <p>There are seven asset classes that can be traded: Stocks, Indices, ETF's, Forex and Commodities (Oil, Gas, Metals etc). Each asset class has its benefits and carries its own risk to your trading capital.</p>
+                            <p>Indices Derivatives allow you to trade on the movement of a basket of shares, rather than just one stock.</p>
+                            <p>Forex allows you to trade on the movement of currency pairs and can be a great way to hedge against movements in the real-world currency markets.</p>
+                            <p>Commodities Derivatives allow you to trade on the movement of underlying commodity prices such as Oil and Gas and can be a great way to diversify your portfolio.</p>
+                            <p>Stocks Derivatives allow you to trade on the movement of individual shares and can be a great way to get exposure to the underlying companies.</p>
+                        </div>
+                    </details>
+                    <details>
                         <summary>What are the steps involved in Derivatives Trading?
                             <span className='blue'>
                                 <DropIcon />
@@ -89,31 +114,6 @@ function IndicesFAQ()
                             <p><strong>High Volatility.</strong> Volatility in Forex Markets: Since there is a high volume of currency trades each day the price movements of some currencies can be very volatile. While this volatility can be advantageous since you can trade price movements in any direction it's also important to employ risk management tools to protect yourself against losses.</p>
                             <p><strong>Trade on leverage.</strong>FlowBank gives you the opportunity to trade Forex with CFDs. When trading CFDs, investors are able to profit from both rising and falling markets. This is due to the ability to both buy (go long) and sell (go short) when trading contracts for difference, providing traders with additional ways to earn a profit when compared with traditional methods of investment.</p>
                             <p><strong>Extensive choice of currency pairs.</strong>A wide range of currency pairs: Forex trading gives you the opportunity to trade global currencies and react to current market events.</p>
-                        </div>
-                    </details>
-                    <details>
-                        <summary>How do Derivatives work?
-                            <span className='blue'>
-                                <DropIcon />
-                            </span>
-                        </summary>
-                        <div className='contt'>
-                            <p>When you trade Derivatives, you don’t actually own the underlying asset. Instead, you are entering into a financial contract to speculate on how high or low the price of the asset will go. If the price moves in your favour, you will make a profit. The risk is that it can also move against you so you will have a losing trade.</p>
-                            <p>Derivatives are a leveraged product, so you only need to put down a small deposit of money (margin) into your trading account to open a position. Using margin gives you the potential to make a much larger profit (or loss) than if you were to trade the asset outright. The use of leverage means a higher risk of losing more than your initial deposit.</p>
-                        </div>
-                    </details>
-                    <details>
-                        <summary>Which assets can I trade?
-                            <span className='blue'>
-                                <DropIcon />
-                            </span>
-                        </summary>
-                        <div className='contt'>
-                            <p>There are seven asset classes that can be traded: Stocks, Indices, ETF's, Forex and Commodities (Oil, Gas, Metals etc). Each asset class has its benefits and carries its own risk to your trading capital.</p>
-                            <p>Indices Derivatives allow you to trade on the movement of a basket of shares, rather than just one stock.</p>
-                            <p>Forex allows you to trade on the movement of currency pairs and can be a great way to hedge against movements in the real-world currency markets.</p>
-                            <p>Commodities Derivatives allow you to trade on the movement of underlying commodity prices such as Oil and Gas and can be a great way to diversify your portfolio.</p>
-                            <p>Stocks Derivatives allow you to trade on the movement of individual shares and can be a great way to get exposure to the underlying companies.</p>
                         </div>
                     </details>
                 </div>
