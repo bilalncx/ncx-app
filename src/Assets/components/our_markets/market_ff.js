b6165f4db62216d5812b112b6f457326354cf279
mbilawalashraf <48543819+mbilawalashraf@users.noreply.github.com>
2023-05-04 18:16:23 +0400

    usdcusd pair added

--- src/Assets/components/our_markets/market_ff.js
+++ src/Assets/components/our_markets/market_ff.js
@@ -299,21 +299,21 @@ function show(data) {
     const BANDChanged = parseFloat(BAND5).toFixed(4);
 
 	// USDC / USD
-	const BUSDC1 = data.USDC_USD.id;
-	const BUSDC_ID = BUSDC1.replace(/_/g, " / ");  
+	const USDCUSD1 = data.USDC_USD.id;
+	const USDCUSD_ID = USDCUSD1.replace(/_/g, " / ");  
 	
-	const BUSDC2 = data.USDC_USD.last;
-	const BUSDC_Price = parseFloat(BUSDC2);
-	const BUSDC_Pricenew = parseFloat(BUSDC2).toFixed(2);
+	const USDCUSD2 = data.USDC_USD.last;
+	const USDCUSD_Price = parseFloat(USDCUSD2);
+	const USDCUSD_Pricenew = parseFloat(USDCUSD2).toFixed(2);
 	
-	const BUSDC3 = data.USDC_USD.high24hr;
-	const BUSDC_High = parseFloat(BUSDC3).toFixed(4);
+	const USDCUSD3 = data.USDC_USD.high24hr;
+	const USDCUSD_High = parseFloat(USDCUSD3).toFixed(4);
 	
-	const BUSDC4 = data.USDC_USD.low24hr;
-	const BUSDC_Low = parseFloat(BUSDC4).toFixed(4);
+	const USDCUSD4 = data.USDC_USD.low24hr;
+	const USDCUSD_Low = parseFloat(USDCUSD4).toFixed(4);
 
-	const BUSDC5 = data.USDC_USD.percentChange * 100;
-    const BUSDCChanged = parseFloat(BUSDC5).toFixed(4);
+	const USDCUSD5 = data.USDC_USD.percentChange * 100;
+    const USDCUSDChanged = parseFloat(USDCUSD5).toFixed(4);
 
 	// BTC / USD
 	const BTC1 = data.BTC_USD.id;
@@ -1252,6 +1252,38 @@ function show(data) {
 			</td>
 			<td>
 				<h1>$562,226.00</h1>
+			</td>
+		</tr>
+
+		<tr>
+			<td>
+				<span class="star-svg">
+					<svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M8.28912 0.446631C8.48263 -0.148878 9.3251 -0.148876 9.51861 0.446631L11.1774 5.55189C11.2639 5.81822 11.5121 5.99851 11.7921 5.99851H17.1601C17.7863 5.99851 18.0466 6.79977 17.54 7.16781L13.1973 10.323C12.9707 10.4876 12.8759 10.7794 12.9624 11.0457L14.6212 16.151C14.8147 16.7465 14.1332 17.2417 13.6266 16.8736L9.2838 13.7184C9.05725 13.5538 8.75048 13.5538 8.52393 13.7184L4.18114 16.8736C3.67458 17.2417 2.993 16.7465 3.18648 16.151L4.84528 11.0457C4.93181 10.7794 4.83701 10.4876 4.61048 10.323L0.267682 7.16781C-0.238887 6.79977 0.0214564 5.99851 0.64761 5.99851H6.0156C6.29563 5.99851 6.5438 5.81822 6.63032 5.55189L8.28912 0.446631Z" fill="#C6C6D3"/>
+					</svg>
+				</span>
+				<h1>${USDCUSD_ID}</h1>
+			</td>
+			<td>
+				<h1>${USDCUSD_Price}</h1>
+				<p>≈$${USDCUSD_Pricenew}</p>
+			</td>
+			<td>
+				<h1>${USDCUSDChanged}%</h1>
+			</td>
+			<td>
+				<h1>${USDCUSD_High}</h1>
+				<p>≈$${USDCUSD_High}</p>
+			</td>
+			<td>
+				<h1>${USDCUSD_Low}</h1>
+				<p>≈$${USDCUSD_Low}</p>
+			</td>
+			<td>
+				<img src="https://ncx.cx/images/new-graphs/dash_usdt.png" alt="graph" />
+			</td>
+			<td>
+				<h1>$1,108,348.00</h1>
 			</td>
 		</tr>
 
