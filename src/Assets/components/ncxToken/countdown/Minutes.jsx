d2514ed82b65b15e898e6639fa5d59ff7d8857ac
mbilawalashraf <48543819+mbilawalashraf@users.noreply.github.com>
2023-05-15 11:49:24 +0400

    ncxt token file changes

--- src/Assets/components/ncxToken/countdown/Minutes.jsx
+++ src/Assets/components/ncxToken/countdown/Minutes.jsx
@@ -12,20 +12,20 @@
     var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-        
+
     // Result is output to the specific element
     document.getElementById("days").innerHTML = days
     document.getElementById("hours").innerHTML = hours
     document.getElementById("mins").innerHTML = minutes
-    document.getElementById("secs").innerHTML = seconds 
+    document.getElementById("secs").innerHTML = seconds
         
     // Display the message when countdown is over
     if (timeleft < 0) {
         clearInterval(myfunc);
-        document.getElementById("days").innerHTML = ""
-        document.getElementById("hours").innerHTML = "" 
-        document.getElementById("mins").innerHTML = ""
-        document.getElementById("secs").innerHTML = ""
+        document.getElementById("days").innerHTML = "";
+        document.getElementById("hours").innerHTML = ""; 
+        document.getElementById("mins").innerHTML = "";
+        document.getElementById("secs").innerHTML = "";
         document.getElementById("end").innerHTML = "TIME UP!!";
     }
     }, 1000);
