9ba72e8a200a417a2e54c684cf55893febf451f3
mbilawalashraf <48543819+mbilawalashraf@users.noreply.github.com>
2023-06-15 17:48:42 +0400

    changes

--- src/Assets/components/homepage/banner/ban.jsx
+++ src/Assets/components/homepage/banner/ban.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import './ban.css';
+import './part.css';
 import Button from "@mui/material/Button";
 import { useTranslation } from "react-i18next";
 import CardLogoSlider from "../../cardpage/newcardpagedesign/scroll_set";
+import Promo1 from '../../../images/promo-1.png';
+import Promo2 from '../../../images/promo-2.png';
+import Promo3 from '../../../images/promo-3.png';
+import Promo4 from '../../../images/promo-4.png';
+import Promo5 from '../../../images/promo-5.png';
+import Promo6 from '../../../images/promo-6.png';
+import Promo7 from '../../../images/promo-7.png';
+import Promo8 from '../../../images/promo-8.png';
+import Promo9 from '../../../images/promo-9.png';
+import Promo10 from '../../../images/promo-10.png';
 
 function HomeBannerSection()
 {
@@ -17,6 +28,151 @@ function HomeBannerSection()
             </div>
 
             <CardLogoSlider />
+
+            <div id="single__part__row" className="mb__partner">
+                <div className="sliders-wrap">
+                    <div className="rooted">
+                        <div className="scroll">
+                            <div className="part">
+                                <div className="box-slide">
+                                    <div className="slider-card">
+                                        <img src={Promo1} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo2} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo3} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo4} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo5} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo6} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo7} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo8} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo9} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo10} alt="ncx"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="part">
+                                <div className="box-slide">
+                                    <div className="slider-card">
+                                        <img src={Promo1} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo2} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo3} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo4} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo5} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo6} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo7} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo8} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo9} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo10} alt="ncx"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="part">
+                                <div className="box-slide">
+                                    <div className="slider-card">
+                                        <img src={Promo1} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo2} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo3} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo4} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo5} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo6} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo7} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo8} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo9} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo10} alt="ncx"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="part">
+                                <div className="box-slide">
+                                    <div className="slider-card">
+                                        <img src={Promo1} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo2} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo3} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo4} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo5} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo6} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo7} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo8} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo9} alt="ncx"/>
+                                    </div>
+                                    <div className="slider-card">
+                                        <img src={Promo10} alt="ncx"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             <div className="btn__dtr">
                 <Button href="/buy-crypto" className="byyy">{t("buy_crypto")}</Button>
