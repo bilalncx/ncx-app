310ddc3011f1a6a43284c2145ab936e9ae5ff0c6
Muhammad Bilawal <bilawal@ncx.cx>
2023-07-24 19:13:52 +0400

    ncx wallet page update

--- src/Assets/components/wallet/01/new.jsx
+++ src/Assets/components/wallet/01/new.jsx
@@ -13,7 +13,7 @@ function NCXAPPSection()
     return(
         <div id="wallet_main_screen" className="hom__ppo">
             <div id="home__ffgrr" className="ncxapp-section">
-                <div id="ncx_buycrypto_video">
+                {/* <div id="ncx_buycrypto_video">
                     <div className="desktop">
                         <video 
                             autoPlay={true}
@@ -25,8 +25,8 @@ function NCXAPPSection()
                             <source src="https://res.cloudinary.com/dqbcnwbvy/video/upload/v1689415590/wallet-video_rq6b91.mp4" type="video/mp4" />
                         </video>
                     </div>
-                </div>
-                <div id="buycrypto_container" className="container">
+                </div> */}
+                <div className="container">
                     <div className="row">
                         <div className="col">
                             <div className="apps_barcod">
@@ -55,7 +55,17 @@ function NCXAPPSection()
                                 </div>
                             </div>
                         </div>
-                        <div className="col"></div>
+                        <div className="col ml__bgvv">
+                            <video 
+                                autoPlay={true}
+                                muted={true}
+                                loop={true}
+                                playsInline={true}
+                                preload="auto"
+                                >
+                                <source src="https://res.cloudinary.com/dqbcnwbvy/video/upload/v1690184914/ncx-derivatives_1_jlfpwu.mp4" type="video/mp4" />
+                            </video>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -67,13 +77,15 @@ function NCXAPPSection()
                     playsInline={true}
                     preload="auto"
                     >
-                    <source src="https://res.cloudinary.com/dqbcnwbvy/video/upload/v1689415590/wallet-video_rq6b91.mp4" type="video/mp4" />
+                    <source src="https://res.cloudinary.com/dqbcnwbvy/video/upload/v1690184914/ncx-derivatives_1_jlfpwu.mp4" type="video/mp4" />
                 </video>
-                <h1>{t("ncx_defi_wallet")}</h1>
-                <p>{t("store_send_swap_mobile_text")}</p>
-                <div className="home__btnff">
-                    <Button href="#" className="sin__op">{t("sign_up")}</Button>
-                    <Button href="#" className="cl__drp">{t("claim_airdrop")}</Button>
+                <div className="fv__rft">
+                    <h1>{t("ncx_defi_wallet")}</h1>
+                    <p>{t("store_send_swap_mobile_text")}</p>
+                    <div className="home__btnff">
+                        <Button href="#" className="sin__op">{t("sign_up")}</Button>
+                        <Button href="#" className="cl__drp">{t("claim_airdrop")}</Button>
+                    </div>
                 </div>
             </div>
         </div>
