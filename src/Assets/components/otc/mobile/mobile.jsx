c86c42699b3e50215d86d596d285fc6fa494c08f
Muhammad Bilawal <bilawal@ncx.cx>
2023-07-25 16:36:50 +0400

    OTC mobile update

---
+++ src/Assets/components/otc/mobile/mobile.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import '../../institution/mobile/mobile.css';
+import Button from "@mui/material/Button";
+import { useTranslation } from "react-i18next";
+import HomeMobilePartner from "../../homepage/banner/partner";
+
+function OTCMobileVersion()
+{
+    const { t } = useTranslation();
+
+    return(
+        <div className="mbl__version">
+            <div className="zx__zx">
+                <video 
+                    autoPlay={true}
+                    muted={true}
+                    loop={true}
+                    playsInline={true}
+                    preload="auto"
+                    >
+                    <source src="https://res.cloudinary.com/dqbcnwbvy/video/upload/v1690184914/ncx-derivatives_1_jlfpwu.mp4" type="video/mp4" />
+                </video>
+                <div className="zx__txt">
+                    <h2>Large trades with NCX OTC Desk</h2>
+                    <p>{t("why_choose_otc_text_1")}</p>
+                    <p>{t("why_choose_otc_text_2")}</p>
+                    <p>{t("why_choose_otc_text_3")}</p>
+                    <Button className="btn__mail" href="mailto: support@ncx.cx">{t("apply")}</Button>
+                </div>
+            </div>
+
+            <div className="zx__zx2 brd__btm">
+                <div className="xs__edf">
+                    <h3>{t("security")}</h3>
+                    <p>{t("security_text")}</p>
+                </div>
+                <div className="xs__edf">
+                    <h3>{t("regulatory_compliance")}</h3>
+                    <p>{t("trading_advantages_text1")}</p>
+                </div>
+                <div className="xs__edf">
+                    <h3>{t("zero_fees")}</h3>
+                    <p>{t("zero_fees_text")}</p>
+                </div>
+                <div className="xs__edf">
+                    <h3>{t("fast_settlement")}</h3>
+                    <p>{t("fast_settlement_text")}</p>
+                </div>
+                <div className="xs__edf">
+                    <h3>{t("wide_coverage")}</h3>
+                    <p>{t("wide_coverage_text")}</p>
+                </div>
+                <div className="xs__edf">
+                    <h3>{t("professional_service")}</h3>
+                    <p>{t("professional_service_text")}</p>
+                </div>
+            </div>
+
+            <div className="zx__zx2 brd__btm">
+                <h2>{t("why_choose_otc")}</h2>
+                <div className="ml__huc">
+                    <img src="https://res.cloudinary.com/dqbcnwbvy/image/upload/v1690288110/block-min_sptnmp.png" alt="ncx-cx"/>
+                    <h3>{t("block_trades")}</h3>
+                    <p>{t("block_trades_text")}</p>
+                </div>
+                <div className="ml__huc">
+                    <img src="https://res.cloudinary.com/dqbcnwbvy/image/upload/v1690288110/rates-min_gsgj2p.png" alt="ncx-cx"/>
+                    <h3>{t("competitive_rates")}</h3>
+                    <p>{t("competitive_rates_text")}</p>
+                </div>
+                <div className="ml__huc">
+                    <img src="https://res.cloudinary.com/dqbcnwbvy/image/upload/v1690288110/settlement-min_fefgmo.png" alt="ncx-cx"/>
+                    <h3>{t("fast_settlement")}</h3>
+                    <p>{t("otc_fast_settlement_text")}</p>
+                </div>
+            </div>
+
+            <div className="zx__zx otc__pag">
+                <HomeMobilePartner />
+            </div>
+        </div>
+    )
+}
+
+export default OTCMobileVersion;
--- src/Assets/pages/ncxotcpage/otc.jsx
+++ src/Assets/pages/ncxotcpage/otc.jsx
@@ -2,55 +2,23 @@ import React from "react";
 import './otc.css';
 import MyHeader from '../../components/header/header';
 import MyFooter from '../../components/footer/footer';
-// import BackVideo from '../../images/ncx-firstslidervideo.mp4';
-// import {ReactComponent as Icon} from '../../images/ncx-staricon.svg';
-// import OTCSecrtion from '../../components/homepage/NCXOTC/otc';
-// import Button from "@mui/material/Button";
-// import { useTranslation } from "react-i18next";
 import NCXOTC1 from '../../components/otc/1/first';
+import OTCMobileVersion from "../../components/otc/mobile/mobile";
 
 function NCXOTCPage()
 {
-
-    // const { t } = useTranslation();
 
     return(
         <div id="ncxotc-page">
             <MyHeader />
 
-            <NCXOTC1 />
+            <div className="otc__desktop">
+                <NCXOTC1 />
+            </div>
 
-            {/* <div className="OTCwelcome">
-                <div className="welcomevideo">
-                    <video
-                        autoPlay={true}
-                        muted={true}
-                        loop={true}
-                        playsInline={true}
-                        >
-                        <source src={BackVideo} type="video/mp4" />
-                    </video>
-                </div>
-                <div className="container">
-                    <div className="welcomeotc row">
-                        <div className="col">
-                            <Icon />
-                            <h1>{t("welcome_ncx_otc")}</h1>
-                            <p>{t("otc_text")}</p>
-                            <Button href="mailto:support@ncx.cx">{t("contact_us")}</Button>
-                        </div>
-                        <div className="col why-choose-otc">
-                            <h1>{t("why_choose_otc")}</h1>
-                            <p>{t("why_choose_otc_text_1")}</p>
-                            <p>{t("why_choose_otc_text_2")}</p>
-                            <p>{t("why_choose_otc_text_3")}</p>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
-
-            {/* <OTCSecrtion /> */}
-
+            <div className="otc__mobile">
+                <OTCMobileVersion />
+            </div>
             <MyFooter />
         </div>
     )
