d3987351574a9b45f60f32ad2ba2c00b3847ad13
Muhammad Bilawal <bilawal@ncx.cx>
2023-08-06 17:52:43 +0400

    Blogs update

--- src/Assets/components/homepage/1stsection/bb_video.jsx
+++ src/Assets/components/homepage/1stsection/bb_video.jsx
@@ -1,57 +1,14 @@
 import React from "react";
 import '../sect_poweredby/powered.css';
-import NCXBarCode from '../../../images/ncx-barcode.png';
-import { ReactComponent as NCXGoogle } from '../../../images/ncxgoogleplay.svg';
-import { ReactComponent as NCXApple } from '../../../images/ncxappstore.svg';
-// import BuyCryptoVideo from '../../../images/home__newvideo.mp4';
 import Button from "@mui/material/Button";
 import './firstsection.css';
 import { useTranslation } from "react-i18next";
-// import NewCardVideo from '../../../images/new-cardvideo.mp4';
 
 function NCXAPPSection()
 {
     const { t } = useTranslation();
     return(
         <div className="hom__ppo">
-            {/* <div id="home__ffgrr" className="ncxapp-section">
-                <div id="ncx_buycrypto_video">
-                    <div className="desktop">
-                        <video 
-                            autoPlay={true}
-                            muted={true}
-                            loop={true}
-                            playsInline={true}
-                            preload="auto"
-                            >
-                            <source src={NewCardVideo} type="video/mp4" />
-                        </video>
-                    </div>
-                </div>
-                <div id="buycrypto_container" className="container">
-                    <div className="row">
-                        <div className="col">
-                            <div className="apps_barcod">
-                                <h1>{t("ncx_exchange_all_in_one_crypto_exchange")}</h1>
-                                <div className="barcodd">
-                                    <img src={NCXBarCode} alt="NCX Bard Code to Download app" />
-                                    <p>{t("scan_to_download_ncx_app")}</p>
-                                </div>
-                                <div className="apps-divi">
-                                    <NCXApple />
-                                    <a href="https://play.google.com/store/apps/details?id=com.mfinance.ncxdigitalexchange.prd" rel="noreferrer" target="_blank"><NCXGoogle /></a>
-                                </div>
-                                <div className="home__btnff">
-                                    <Button href="https://ncx.cx/register" rel="noreferrer" target="_blank" className="sin__op">{t("sign_up")}</Button>
-                                    <Button href="#" className="cl__drp">{t("claim_airdrop")}</Button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col"></div>
-                    </div>
-                </div>
-            </div> */}
-            
             <div className="new__crf">
             <div className="bgt_hyn">
                 <video 
--- src/Assets/components/homepage/2ndsection/2ndsection.jsx
+++ src/Assets/components/homepage/2ndsection/2ndsection.jsx
@@ -1,122 +1,119 @@
-import  { React, useState } from "react";
-import { AnimationOnScroll } from 'react-animation-on-scroll';
-import { ReactComponent as Icon1 } from '../../../images/icon-1.svg';
-import { ReactComponent as Icon2 } from '../../../images/icon-2.svg';
-import { ReactComponent as Icon3 } from '../../../images/icon-3.svg';
-import { ReactComponent as Icon4 } from '../../../images/icon-4.svg';
-import './2ndsection.css';
+// import  { React, useState } from "react";
+// import { AnimationOnScroll } from 'react-animation-on-scroll';
+// import { ReactComponent as Icon1 } from '../../../images/icon-1.svg';
+// import { ReactComponent as Icon2 } from '../../../images/icon-2.svg';
+// import { ReactComponent as Icon3 } from '../../../images/icon-3.svg';
+// import { ReactComponent as Icon4 } from '../../../images/icon-4.svg';
+// import './2ndsection.css';
 
-function Home2ndSection(){
-    return(
-        <div id="cex" className="2ndsect">
-                <div className="second-container">
-                    <div className="row">
-                        <div className="cex asss">
-                            <div className="animtation">
-                                <AnimationOnScroll animateIn="animate__fadeIn">
-                                    <h1>Hybrid CEX DEX all-in-one</h1>
-                                </AnimationOnScroll>
-                            </div>
-                            <div className="animtation">
-                                <AnimationOnScroll animateIn="animate__fadeIn">
-                                    <h1>Cryptocurrency Exchange</h1>
-                                </AnimationOnScroll>
-                            </div>
-                            <div className="animtation">
-                                {/* <AnimationOnScroll animateIn="animate__slideInUp">
-                                    <h1>& Trading Platform</h1>
-                                </AnimationOnScroll> */}
-                                <AnimationOnScroll animateIn="animate__fadeIn">
-                                    <h1>for Web3.</h1>
-                                </AnimationOnScroll>
-                            </div>
-                        </div>
-                        <div className="dex">
-                            <AnimationOnScroll animateIn="animate__fadeIn">
-                                <p>NCX is a next generation Hybrid Cryptocurrency Exchange combining Decentralised Finance 
-                                    with the ease of use & advantages of a traditional Cryptocurrency Exchange in a single 
-                                    advanced all-in-one  platform and scalable ecosystem, providing an ultimate Cryptocurrency 
-                                    Exchange for Web3. Having the main benefits of CEX such as fiat deposits/withdrawals, advanced 
-                                    spot trading with multiple order types, high trading volume and liquidity, fiat-cryptocurrency 
-                                    trading and vice versa, advanced security and 24/7 live support. </p>
-                            </AnimationOnScroll>
-                        </div>
-                    </div>
-                    <div className="row hh_row">
-                        <div className="hh_col">
-                            <AnimationOnScroll animateIn="animate__fadeIn" className="hh-fade1">
-                                <div className="line-div"></div>
-                            </AnimationOnScroll>
-                            <p>01</p>
-                            <div className="animtation">
-                                <AnimationOnScroll animateIn="animate__fadeIn">
-                                    <h1>Advanced CEX 
-                                    <span>Spot Trading</span> 
-                                    <span>Margin Trading</span>
-                                    </h1>
-                                </AnimationOnScroll>
-                            </div>
-                            <div className="hh_icon">
-                                <AnimationOnScroll animateIn="animate__fadeIn">
-                                    <Icon1 />
-                                </AnimationOnScroll>
-                            </div>
-                        </div>
-                        <div className="hh_col">
-                            <AnimationOnScroll animateIn="animate__fadeIn" id="hh-fade2">
-                                <div className="line-div"></div>
-                            </AnimationOnScroll>
-                            <p>02</p>
-                            <div className="animtation">
-                                <AnimationOnScroll animateIn="animate__fadeIn">
-                                    <h1>Multichain DEX 
-                                        <span>on 5 Blockchains</span>
-                                    </h1>
-                                </AnimationOnScroll>
-                            </div>
-                            <div className="hh_icon">
-                                <AnimationOnScroll animateIn="animate__fadeIn">
-                                    <Icon2 />
-                                </AnimationOnScroll>
-                            </div>
-                        </div>
-                        <div className="hh_col">
-                            <AnimationOnScroll animateIn="animate__fadeIn" id="hh-fade3">
-                                <div className="line-div"></div>
-                            </AnimationOnScroll>
-                            <p>03</p>
-                            <div className="animtation">
-                                <AnimationOnScroll animateIn="animate__fadeIn">
-                                    <h1>500 Derivatives 
-                                        <span>Trading Pairs</span></h1>
-                                </AnimationOnScroll>
-                            </div>
-                            <div className="hh_icon">
-                                <AnimationOnScroll animateIn="animate__fadeIn">
-                                    <Icon3 />
-                                </AnimationOnScroll>
-                            </div>
-                        </div>
-                        <div className="hh_col">
-                            <AnimationOnScroll animateIn="animate__fadeIn" id="hh-fade4">
-                                <div className="line-div"></div>
-                            </AnimationOnScroll>
-                            <p>04</p>
-                            <div className="animtation">
-                                <AnimationOnScroll animateIn="animate__fadeIn">
-                                    <h1>DeFi Staking 
-                                        <span>Yield Farming</span></h1>
-                                </AnimationOnScroll>
-                            </div>
-                            <div className="hh_icon">
-                                <AnimationOnScroll animateIn="animate__fadeIn">
-                                    <Icon4 />
-                                </AnimationOnScroll>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    )
-}
-export default Home2ndSection;
+// function Home2ndSection(){
+//     return(
+//         <div id="cex" className="2ndsect">
+//                 <div className="second-container">
+//                     <div className="row">
+//                         <div className="cex asss">
+//                             <div className="animtation">
+//                                 <AnimationOnScroll animateIn="animate__fadeIn">
+//                                     <h1>Hybrid CEX DEX all-in-one</h1>
+//                                 </AnimationOnScroll>
+//                             </div>
+//                             <div className="animtation">
+//                                 <AnimationOnScroll animateIn="animate__fadeIn">
+//                                     <h1>Cryptocurrency Exchange</h1>
+//                                 </AnimationOnScroll>
+//                             </div>
+//                             <div className="animtation">
+//                                 <AnimationOnScroll animateIn="animate__fadeIn">
+//                                     <h1>for Web3.</h1>
+//                                 </AnimationOnScroll>
+//                             </div>
+//                         </div>
+//                         <div className="dex">
+//                             <AnimationOnScroll animateIn="animate__fadeIn">
+//                                 <p>NCX is a next generation Hybrid Cryptocurrency Exchange combining Decentralised Finance 
+//                                     with the ease of use & advantages of a traditional Cryptocurrency Exchange in a single 
+//                                     advanced all-in-one  platform and scalable ecosystem, providing an ultimate Cryptocurrency 
+//                                     Exchange for Web3. Having the main benefits of CEX such as fiat deposits/withdrawals, advanced 
+//                                     spot trading with multiple order types, high trading volume and liquidity, fiat-cryptocurrency 
+//                                     trading and vice versa, advanced security and 24/7 live support. </p>
+//                             </AnimationOnScroll>
+//                         </div>
+//                     </div>
+//                     <div className="row hh_row">
+//                         <div className="hh_col">
+//                             <AnimationOnScroll animateIn="animate__fadeIn" className="hh-fade1">
+//                                 <div className="line-div"></div>
+//                             </AnimationOnScroll>
+//                             <p>01</p>
+//                             <div className="animtation">
+//                                 <AnimationOnScroll animateIn="animate__fadeIn">
+//                                     <h1>Advanced CEX 
+//                                     <span>Spot Trading</span> 
+//                                     <span>Margin Trading</span>
+//                                     </h1>
+//                                 </AnimationOnScroll>
+//                             </div>
+//                             <div className="hh_icon">
+//                                 <AnimationOnScroll animateIn="animate__fadeIn">
+//                                     <Icon1 />
+//                                 </AnimationOnScroll>
+//                             </div>
+//                         </div>
+//                         <div className="hh_col">
+//                             <AnimationOnScroll animateIn="animate__fadeIn" id="hh-fade2">
+//                                 <div className="line-div"></div>
+//                             </AnimationOnScroll>
+//                             <p>02</p>
+//                             <div className="animtation">
+//                                 <AnimationOnScroll animateIn="animate__fadeIn">
+//                                     <h1>Multichain DEX 
+//                                         <span>on 5 Blockchains</span>
+//                                     </h1>
+//                                 </AnimationOnScroll>
+//                             </div>
+//                             <div className="hh_icon">
+//                                 <AnimationOnScroll animateIn="animate__fadeIn">
+//                                     <Icon2 />
+//                                 </AnimationOnScroll>
+//                             </div>
+//                         </div>
+//                         <div className="hh_col">
+//                             <AnimationOnScroll animateIn="animate__fadeIn" id="hh-fade3">
+//                                 <div className="line-div"></div>
+//                             </AnimationOnScroll>
+//                             <p>03</p>
+//                             <div className="animtation">
+//                                 <AnimationOnScroll animateIn="animate__fadeIn">
+//                                     <h1>500 Derivatives 
+//                                         <span>Trading Pairs</span></h1>
+//                                 </AnimationOnScroll>
+//                             </div>
+//                             <div className="hh_icon">
+//                                 <AnimationOnScroll animateIn="animate__fadeIn">
+//                                     <Icon3 />
+//                                 </AnimationOnScroll>
+//                             </div>
+//                         </div>
+//                         <div className="hh_col">
+//                             <AnimationOnScroll animateIn="animate__fadeIn" id="hh-fade4">
+//                                 <div className="line-div"></div>
+//                             </AnimationOnScroll>
+//                             <p>04</p>
+//                             <div className="animtation">
+//                                 <AnimationOnScroll animateIn="animate__fadeIn">
+//                                     <h1>DeFi Staking 
+//                                         <span>Yield Farming</span></h1>
+//                                 </AnimationOnScroll>
+//                             </div>
+//                             <div className="hh_icon">
+//                                 <AnimationOnScroll animateIn="animate__fadeIn">
+//                                     <Icon4 />
+//                                 </AnimationOnScroll>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//     )
+// }
+// export default Home2ndSection;
